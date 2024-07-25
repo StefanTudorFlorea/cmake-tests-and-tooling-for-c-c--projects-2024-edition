1f4626a6660be827bc9f9ca22a715e53513f9d1d
sfl <stefanflorea.tudor@gmail.com>
2024-07-25 16:35:02 +0200

    .

--- cmake/AddGitSubmodule.cmake
+++ cmake/AddGitSubmodule.cmake
@@ -1,17 +1,30 @@
+
 function(add_git_submodule git dest)
     message("Going to work...")
+
+    # clone the submodule
     find_package(Git REQUIRED)
 
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} submodule add ${PROJECT_SOURCE_DIR}/${dest}
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    )
+    message("Path1: ${CMAKE_SOURCE_DIR}")
+    message("dest: ${dest}")
 
-    if (NOT EXISTS ${dir}/CMakeLists.txt)
-        message("Submodule does not have a CMakeLists.txt")
-        return()
+    if (EXISTS ${CMAKE_SOURCE_DIR}/${dest})
+        message("HERE---------------")
     endif()
 
-    add_subdirectory(${dir})
+    # add submodule if not already there
+    # if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${dest})
+    #     execute_process(
+    #         COMMAND ${GIT_EXECUTABLE} submodule add ${git} ${CMAKE_SOURCE_DIR}/${dest}
+    #         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+    # endif()
+
+    # # make sure we can include it in our cmake
+    # if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${dest}/CMakeLists.txt)
+    #     message("Submodule does not have a CMakeLists.txt")
+    #     return()
+    # endif()
+
+    add_subdirectory(${CMAKE_SOURCE_DIR}/${dest} ${CMAKE_SOURCE_DIR}/build/${dest})
 
 endfunction()
