1fe961f8a02cd36c4784b89d03203f2ee5be4c1a
sfl <stefanflorea.tudor@gmail.com>
2024-07-25 19:22:14 +0200

    Add doxygen

--- demos/6-documentation/src/main.cpp
+++ demos/6-documentation/src/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string>
+
+   
+/// @brief Print a message to the screen
+/// @param txt Message to print
+void helloWorld(const std::string& txt) {
+
+}
 
 int main() {
 }
